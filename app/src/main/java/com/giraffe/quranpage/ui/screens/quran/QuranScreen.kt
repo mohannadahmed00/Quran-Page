edb2f410b8d771bb926eced3a331d34deda95c9b
mohannadahmed00 <mohannad199344@gmail.com>
2024-06-17 03:25:48 +0200

    quran assets have been enhanced

--- app/src/main/java/com/giraffe/quranpage/ui/screens/quran/QuranScreen.kt
+++ app/src/main/java/com/giraffe/quranpage/ui/screens/quran/QuranScreen.kt
@@ -1,23 +1,13 @@
 package com.giraffe.quranpage.ui.screens.quran
 
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import android.util.Log
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
@@ -35,6 +25,4 @@ fun QuranContent(
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center){
         Text(text = "hello")
     }
-
-
 }
