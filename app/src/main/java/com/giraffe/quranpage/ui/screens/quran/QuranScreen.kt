5020fc17745e683cc350080fc6413a282bdd1ccc
mohannadahmed00 <mohannad199344@gmail.com>
2024-07-19 04:18:54 +0200

    bottom sheets of options and tafsir have been handled

--- app/src/main/java/com/giraffe/quranpage/ui/screens/quran/QuranScreen.kt
+++ app/src/main/java/com/giraffe/quranpage/ui/screens/quran/QuranScreen.kt
@@ -6,7 +6,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -16,27 +18,41 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDirection
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.giraffe.quranpage.ui.composables.Page
 import com.giraffe.quranpage.ui.theme.brown
 import com.giraffe.quranpage.ui.theme.cream
+import com.giraffe.quranpage.ui.theme.fontFamilies
 import com.giraffe.quranpage.ui.theme.transparent
+import ir.kaaveh.sdpcompose.ssp
 import kotlinx.coroutines.launch
 
 @Composable
@@ -47,7 +63,7 @@ fun QuranScreen(
     QuranContent(state, viewModel)
 }
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun QuranContent(
     state: QuranScreenState = QuranScreenState(),
@@ -108,6 +124,9 @@ fun QuranContent(
             }
         }
     ) {
+        var showOptionsBottomSheet by remember { mutableStateOf(false) }
+        var showTafsirBottomSheet by remember { mutableStateOf(false) }
+
         HorizontalPager(
             state = pagerState,
             reverseLayout = true,
@@ -115,9 +134,79 @@ fun QuranContent(
             Page(
                 modifier = Modifier.fillMaxSize(),
                 pageUI = state.pages[page],
-                onVerseSelected = events::onVerseSelected,
+                onVerseSelected = { a, s, c ->
+                    events.onVerseSelected(a, s, c)
+                    showOptionsBottomSheet = true
+                },
                 onDrawerClicked = { scope.launch { drawerState.open() } }
             )
+        }
+        if (showOptionsBottomSheet) {
+            ModalBottomSheet(
+                modifier = Modifier.fillMaxHeight(),
+                sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = false),
+                onDismissRequest = { showOptionsBottomSheet = false }
+            ) {
+                val surah = state.surahesData[(state.selectedVerse?.surahNumber?.minus(1)) ?: 0]
+                Text(
+                    "Surah (${surah.name}) Index : ${surah.id}\nVrese Index : ${state.selectedVerse?.verseNumber ?: 0}",
+                    style = TextStyle(color = MaterialTheme.colorScheme.onPrimary, fontSize = 16.ssp),
+                    modifier = Modifier.padding(16.dp)
+                )
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(containerColor = brown.copy(alpha = 0.1f)),
+                    onClick = {
+                        showTafsirBottomSheet = true
+                    }) {
+                    Text("Tafsir")
+                }
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(containerColor = brown.copy(alpha = 0.2f)),
+                    onClick = {
+                        showTafsirBottomSheet = true
+                    }) {
+                    Text("Share")
+                }
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(containerColor = brown.copy(alpha = 0.3f)),
+                    onClick = {
+                        showTafsirBottomSheet = true
+                    }) {
+                    Text("Listen")
+                }
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = ButtonDefaults.buttonColors(containerColor = brown.copy(alpha = 0.4f)),
+                    onClick = {
+                        showTafsirBottomSheet = true
+                    }) {
+                    Text("Bookmark")
+                }
+
+            }
+        }
+        if (showTafsirBottomSheet) {
+            ModalBottomSheet(
+                modifier = Modifier.fillMaxHeight(),
+                sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true),
+                onDismissRequest = { showTafsirBottomSheet = false }
+            ) {
+                Text(
+                    state.selectedVerse?.qcfData ?: "",
+                    style = TextStyle(
+                        textAlign = TextAlign.Center,
+                        textDirection = TextDirection.ContentOrRtl,
+                        fontFamily = fontFamilies[(state.selectedVerse?.pageIndex ?: 1) - 1],
+                        fontSize = 20.ssp
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp)
+                )
+            }
         }
     }
 }
