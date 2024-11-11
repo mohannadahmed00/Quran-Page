97c39b861867dfb2f63c23c116562acc9a5ddfe9
mohannadahmed00 <mohannad199344@gmail.com>
2024-11-11 23:45:36 +0200

    verses landscape view has been handled

--- app/src/main/java/com/giraffe/quranpage/presentation/ui/composables/Page.kt
+++ app/src/main/java/com/giraffe/quranpage/presentation/ui/composables/Page.kt
@@ -1,10 +1,13 @@
 package com.giraffe.quranpage.presentation.ui.composables
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.giraffe.quranpage.domain.entities.SurahDataEntity
 import com.giraffe.quranpage.domain.entities.VerseEntity
 import com.giraffe.quranpage.presentation.ui.screens.quran.PageUi
@@ -16,26 +19,35 @@ fun Page(
     onVerseSelected: (VerseEntity) -> Unit,
     onPageClick: () -> Unit
 ) {
-    Column(
+    LazyColumn (
+        modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        PageHeader(
-            surahName = pageUI.namesOfSurahes,
-            juz = pageUI.juz
-        )
-        PageContent(
-            modifier = Modifier.weight(1f),
-            pageUI = pageUI,
-            surahesData = surahesData,
-            onVerseSelected = onVerseSelected,
-            onPageClick = onPageClick
-        )
-        PageFooter(
-            pageIndex = pageUI.pageIndex,
-            hezb = pageUI.hezbStr,
-            hasSajdah = pageUI.hasSajdah
-        )
+        item {
+            PageHeader(
+                surahName = pageUI.namesOfSurahes,
+                juz = pageUI.juz
+            )
+        }
+        item{
+            PageContent(
+                modifier = Modifier.width(415.dp),
+                pageUI = pageUI,
+                surahesData = surahesData,
+                onVerseSelected = onVerseSelected,
+                onPageClick = onPageClick
+            )
+        }
+
+        item {
+            PageFooter(
+                pageIndex = pageUI.pageIndex,
+                hezb = pageUI.hezbStr,
+                hasSajdah = pageUI.hasSajdah
+            )
+        }
+
     }
 
 }
